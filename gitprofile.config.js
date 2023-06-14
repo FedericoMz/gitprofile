4aa878b07e7baf703b1d0b2c8b49978de0a2d196
Federico Mazzoni <80719913+FedericoMz@users.noreply.github.com>
2023-06-14 17:02:06 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -158,7 +158,7 @@ const config = {
   // Optional Footer. Supports plain text or HTML.
 
   
-  footer: `Thank you for visiting my website! <p style="text-align:center;"><img src="https://i.imgur.com/DGuPmTn.gi" alt="Bye!"></p>`,
+  footer: `Thank you for visiting my website! <p style="text-align:center;"><img src="https://i.imgur.com/DGuPmTn.gif" alt="Bye!"></p>`,
 };
 
 export default config;
