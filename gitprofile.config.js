07474960f11120e16fc49944ca1caca8b91c5e2e
Federico Mazzoni <80719913+FedericoMz@users.noreply.github.com>
2023-11-27 14:20:43 +0100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -70,7 +70,7 @@ const config = {
   education: [
     {
       institution: 'University of Pisa',
-      degree: 'Master\'s Degree in Digital Humanities <br/> (Data Science and Language Technologies)',
+      degree: 'Master\'s Degree in Digital Humanities (Data Science and NLP)',
       from: '2020',
       to: '2022',
     },
